81da2e3914f1176085a6adfe5b1f7bab93257d09
Yiyoung Liu <yiyoung.md@gmail.com>
2026-03-16 10:46:45 -0400

    feat: start increment 3, up to time based greeting

---
+++ yiyoungl-increment3/static/script.js
@@ -0,0 +1,56 @@
+// var x = 5;
+// var y = 7;
+// var z = x + y;
+// console.log(z);
+//
+// var A = "Hello ";
+// var B = "world!";
+// var C = A + B;
+// console.log(C);
+//
+// function sumPrint(x1, x2) {
+//     console.log(x1 + x2);
+// }
+//
+// sumPrint(x, y);
+// sumPrint(A, B);
+//
+// if (C.length > z) {
+//     console.log(C);
+// } else if (C.length < z) {
+//     console.log(z);
+// } else {
+//     console.log("good job!");
+// }
+//
+// L1 = ["Watermelon", "Pineapple", "Pear", "Banana"];
+// L2 = ["Apple", "Banana", "Kiwi", "Orange"];
+//
+// function findTheBanana(arr) {
+//     arr.forEach((item) => {
+//         item === "Banana" ? alert("Banana") : null;
+//     })
+// }
+//
+// findTheBanana(L1);
+// findTheBanana(L2);
+
+var now = new Date();
+var hour = now.getHours();
+function greeting(x) {
+    var str;
+    if (x < 5) {
+        str = "Good Night";
+    } else if (x < 12) {
+        str = "Good Morning";
+    } else if (x < 18) {
+        str = "Good Afternoon";
+    } else if (x < 20) {
+        str = "Good Evening";
+    } else {
+        str = "Good Night";
+    }
+    document.getElementById("greeting").innerHTML = str + document.getElementById("greeting").innerHTML;
+}
+
+greeting(hour);
